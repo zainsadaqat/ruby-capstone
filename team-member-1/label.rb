84dbd0b7956d20db57fb60adb04355f385327a5b
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-01-21 00:43:04 +0500

    Create label class

---
+++ team-member-1/label.rb
@@ -0,0 +1,28 @@
+class Label < Item
+  attr_accessor :title, :color
+  attr_reader :id
+
+  def initialize(title, color)
+    @id = Random.rand(1...10000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    item.add_label(self)
+    @items.push(self) unless @items.include?(item)
+  end
+
+  def to_s
+    "Title: #{self.title}, Color: #{self.color}"
+  end
+
+  def to_json
+    {
+      'title' => @title,
+      'color' => @color
+    }
+  end
+
+end

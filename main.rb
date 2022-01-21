e863c3f83d8396c62e365c548b3a82769306de1a
iammouaz <88675267+iammouaz@users.noreply.github.com>
2022-01-21 23:30:37 +0200

    Fix linters issues

--- main.rb
+++ main.rb
@@ -14,8 +14,8 @@ class App
     @options = {
       '1' => 'List all books', '2' => 'List all music albums',
       '3' => 'List all movies', '5' => 'List all genres',
-       '6' => 'List all labels', '9' => 'Add a book',
-       '10' => 'Add a music album', '11' => 'Add a movie',
+      '6' => 'List all labels', '9' => 'Add a book',
+      '10' => 'Add a music album', '11' => 'Add a movie',
       '0' => 'Save and exit'
     }
   end
